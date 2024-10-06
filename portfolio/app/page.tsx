cba5a0be7d659fd796a7052543c5e784d3628c94
Danny <gbnguyenw@gmail.com>
2024-10-05 20:34:49 -0400

    Added links to nav bar.

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,11 +1,16 @@
+'use client'
 import Content from "@components/Content";
 import Nav from "@components/Nav";
 import Footer from "@components/Footer";
+import { Box,LinearProgress } from "@mui/material";
 export default function Home() {
   return (
 
       <main>
         <Nav/>
+        <Box sx={{ width: '100%' }}>
+          <LinearProgress />
+        </Box>
         <Content/>
         <Footer/>
       </main>
--- portfolio/components/Nav.tsx
+++ portfolio/components/Nav.tsx
@@ -1,27 +1,94 @@
+'use client'
 import React from 'react'
-import { Box,Toolbar,IconButton,AppBar,Typography,Button } from '@mui/material'
+import { Box,Toolbar,IconButton,AppBar,Container,MenuItem,Menu } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
+
+const settings = ['Home', 'Skills', 'Education', 'Experiences','Contact'];
 const Nav = () => {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-            <Toolbar>
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElNav(event.currentTarget);
+    };
+ 
+    const handleCloseNavMenu = () => {
+      setAnchorElNav(null);
+    };
+  
+    const handleLinkClick = () => {
+        setAnchorElNav(null);
+    };
+    const collapseNavigation = ()=>{
+        // collapse navigation links into menu icon
+        return(
+            <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
                 <IconButton
                   size="large"
-                  edge="start"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
                   color="inherit"
-                  aria-label="menu"
-                  sx={{ mr: 2 }}
                 >
                   <MenuIcon />
                 </IconButton>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                  News
-                </Typography>
-                <Button color="inherit">Login</Button>
-            </Toolbar>
-        </AppBar>
-    </Box>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{ display: { xs: 'block', md: 'none' } }}
+                >
+                  {settings.map((setting) => (
+                        <MenuItem onClick={handleLinkClick} component={'a'} sx={{ textAlign: 'center' }} key={setting} href={`#${setting}`}>
+                            {setting}
+                        </MenuItem>
+                  ))}
+                  <MenuItem component={'a'} sx={{ textAlign: 'center' }} target='_blank' href='https://github.com/DannyNguyenCode'>
+                      Github
+                  </MenuItem>
+                </Menu>
+          </Box>
+        )
+    }
+    const navigationMenu=()=>{
+        return (
+        <Box sx={{  display: { xs: 'none', md: 'flex' } }}>
+              {settings.map((setting) => (
+                <MenuItem component={'a'} sx={{ textAlign: 'center' }} key={setting} href={`#${setting}`}>
+                    {setting}
+                </MenuItem>
+              ))}
+                <MenuItem component={'a'} sx={{ textAlign: 'center' }} target='_blank' rel="noopener noreferrer" href='https://github.com/DannyNguyenCode'>
+                    Github
+                </MenuItem>
+        </Box>
+        )
+    }
+
+  return (
+    <AppBar color='transparent' position="static">
+      <Container maxWidth="xl">
+        <Toolbar sx={{display:'flex',justifyContent:'space-between'}} disableGutters>
+
+            {navigationMenu()}
+
+            {collapseNavigation()}
+            <MenuItem component={'a'} sx={{ textAlign: 'center' }} href='/login'>
+                Admin Login
+            </MenuItem>
+        </Toolbar>
+      </Container>
+    </AppBar>
   )
 }
 
